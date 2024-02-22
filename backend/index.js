1ea03529846a2ec1b63489fb2d499174382847a9
Apekshakini <apekshakini.n@gmail.com>
2024-02-22 17:32:08 +0530

    done

---
+++ backend/index.js
@@ -0,0 +1,44 @@
+import express from "express"
+import cookieParser from "cookie-parser"
+import cors from 'cors'
+import mongoose from "mongoose"
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const app=express();
+const port=process.env.PORT || 8000;
+
+const corsOptions={
+    origin:true
+}
+
+app.get('/',(req,res)=>{
+    res.send('Api is working');
+});
+
+//
+mongoose.set('strictQuery',false)
+const connectDB=async()=>{
+    try{
+        await mongoose.connect(process.env.MONGO_URL,{
+            useNewUrlParser:true,
+            useUnifiedTopology:true,
+        })
+
+        console.log("Mongodb is connected");
+
+    }catch(err){
+        console.log("Mongodb is failed");
+    }
+}
+
+
+app.use(express.json())
+app.use(cookieParser())
+app.use(cors(corsOptions))
+
+app.listen(port,()=>{
+    connectDB();
+    console.log("server is running on the port" + port);
+});
