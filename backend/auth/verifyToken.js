92290727dd7ce7daad1c5f977e36139600ac1d90
Apekshakini <apekshakini.n@gmail.com>
2024-02-27 19:00:02 +0530

    done

--- backend/auth/verifyToken.js
+++ backend/auth/verifyToken.js
@@ -4,14 +4,40 @@ import User from '../models/UserSchema.js'
 
 export const authenticate=async(req,res,next)=>{
     const authToken=req.headers.authorization
-    if(!authToken || !authToken.startWith("Bearer")){
-        return res.status(401).json({success:false,message:'No token ,Autherization denied'});
+    if(!authToken || !authToken.startsWith("Bearer")){
+        return res
+        .status(401)
+        .json({success:false,message:'No token ,Autherization denied'});
     }
     try{
-        console.log(authToken);
+        const token =authToken.split(" ")[1];
+        const decoded=jwt.verify(token,process.env.JWT_SECRET_KEY)
+        req.userId=decoded.id
+        req.role=decoded.role
         next();
 
     }catch(err){
+        if(err.name=='TokenExpiredError'){
+            return res.status(401).json({message:'token is expired'})
+        }
+        return res.status(401).json({success:false,message:'invalid token'})
 
     }
+};
+export const restrict=roles=> async(req,res,next)=>{
+    const userId=req.userId
+    let user;
+    const patient=await User.findById(userId)
+    const doctor=await Doctor.findById(userId)
+    if(patient){
+        user=patient
+    }
+    if(doctor){
+        user=doctor
+    }
+    if (!roles.includes(user.role)){
+        return res.status(401).json({success:false,message:"you are not authorized"})
+    }
+    next();
+
 }
